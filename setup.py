bb4a0d9e96bb857700d64c80d3dcc973d631e52a
elv-joeA <joe.anakata@eluv.io>
2025-09-30 22:50:25 +0000

    Revert "point back at temporary branch of common_ml to get dumb utility function"
    This reverts commit 6007b227eed7f10a25cf9f61a16cc402070ab6ff.
    
    (Erroneous commit caused by not pulling the latest common_ml locally)

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
         'psutil',
         'dacite',
         'elv-client-py @ git+https://github.com/eluv-io/elv-client-py.git#egg=elv-client-py',
-        'common_ml @ git+ssh://git@github.com/eluv-io/common-ml.git@features-for-tagger#egg=common_ml',
+        'common_ml @ git+ssh://git@github.com/eluv-io/common-ml.git#egg=common_ml',
     ]
 )
