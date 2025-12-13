1831375beed05e96c8c11941444162b6b54f3b99
elv-joeA <joe.anakata@eluv.io>
2025-12-13 06:49:11 +0000

    verify that we can get labels off the image, and update to newer build scripts

--- misc/pmclient_list_images.py
+++ misc/pmclient_list_images.py
@@ -2,7 +2,17 @@
 ### easy way to make sure podman client is working
 from podman import PodmanClient
 
+
+## this shows that we get the LABELS but not the attributes
+
 with PodmanClient() as podman_client:
     images =  podman_client.images.list()
     for image in images:
-        print(image)
+        print(image, image.id)
+        ## print("   ", image.attrs)
+        if image.labels.get("org.opencontainers.image.vendor", "") == "Eluvio, Inc.":
+            for key, value in image.labels.items():
+                print(f"      {key}: {value}")
+        else:
+            print("      Not Eluvio, Inc.")
+
