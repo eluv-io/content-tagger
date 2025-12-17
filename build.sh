49f509e8fd9cfe836282ba895d43bdac3c951a7c
elv-joeA <joe.anakata@eluv.io>
2025-12-17 22:34:45 +0000

    follow recommendation of buildscripts and don't update submodule if not needed

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-git submodule update --init --recursive 
+set -x
+
+[ -f buildscripts/README.md ] || git submodule update --init --recursive 
 
 exec buildscripts/build_container.bash -t "content-tagger:${IMAGE_TAG:-latest}" . -f Dockerfile "$@"
