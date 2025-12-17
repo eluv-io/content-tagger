32ecc763e312fd86b29871cd51a5e7e3165a8d31
elv-joeA <joe.anakata@eluv.io>
2025-12-17 23:03:43 +0000

    this has to be done every time (submodules do suck)

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-set -x
-
-[ -f buildscripts/README.md ] || git submodule update --init --recursive 
+git submodule update --init --recursive
 
 exec buildscripts/build_container.bash -t "content-tagger:${IMAGE_TAG:-latest}" . -f Dockerfile "$@"
