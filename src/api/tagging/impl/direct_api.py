f33a07efb9d32754bf8bdd14318e1248a4c790a2
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2026-03-05 22:13:24 +0000

    added direct api impl

---
+++ src/api/tagging/impl/direct_api.py
@@ -0,0 +1,17 @@
+from src.common.content import Content
+from src.tagging.fabric_tagging.model import TagArgs, TagJobStatusReport, TagStartResult, TagStopResult
+from src.tagging.fabric_tagging.tagger import FabricTagger
+from src.api.tagging.abstract import TagAPI
+
+class DirectAPI(TagAPI):
+    def __init__(self, tagger: FabricTagger):
+        self.tagger = tagger
+
+    def tag(self, q: Content, args: TagArgs) -> TagStartResult:
+        return self.tagger.tag(q, args)
+
+    def status(self, qhit: str) -> list[TagJobStatusReport]:
+        return self.tagger.status(qhit)
+
+    def stop(self, qhit: str, feature: str | None, stream: str | None) -> list[TagStopResult]:
+        return self.tagger.stop(qhit, feature, stream)
